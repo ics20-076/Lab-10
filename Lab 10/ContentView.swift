f6c225c170da4cb265d47c4c3fc29f938990bb27
Emily <ics20-076@camosun.bc.ca>
2021-03-22 15:51:13 -0700

    Initial Commit

---
+++ Lab 10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab 10
+//
+//  Created by ICS 224 on 2021-03-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab 10/Lab_10App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_10App.swift
+//  Lab 10
+//
+//  Created by ICS 224 on 2021-03-22.
+//
+
+import SwiftUI
+
+@main
+struct Lab_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
