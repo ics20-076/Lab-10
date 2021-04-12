07c58649475f652f9c9e57a8152d40b0515ee273
ics20-076 <ics20-076@camosun.bc.ca>
2021-04-12 13:26:23 -0700

    finished this fish

--- Lab 10/mapView.swift
+++ Lab 10/mapView.swift
@@ -10,12 +10,11 @@ import MapKit
 
 struct MapItems{
     var items = [MapItem(latitude: 48.4897, longitude: -123.4160, label: "Camosun Interurban"), MapItem(latitude: 48.4634, longitude: -123.3117, label: "UVIC"), MapItem(latitude: 49.1576, longitude: -123.9670, label: "VIU")]
-    //Camosun Int. 48.4897, -123.4160
 }
 //Root view contains list of coordinates
 struct MapView: View {
     @StateObject var locationManager = LocationManager()
-    @State var trackingMode = MapUserTrackingMode.follow
+    @State var trackingMode = MapUserTrackingMode.none
     var mapItems = MapItems()
     
     var body: some View {
@@ -32,7 +31,10 @@ struct MapView: View {
                        tint: .green)
             }
             VStack{
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Button(action: {
+                    //Reset region to user location
+                    trackingMode = MapUserTrackingMode.follow
+                }, label: {
                     Image(systemName: "house.fill")
                         .padding(5)
                         .overlay(
@@ -43,7 +45,10 @@ struct MapView: View {
                 List {
                     ForEach(mapItems.items) { item in
                         Button(action: {
-                            
+                            trackingMode = MapUserTrackingMode.none
+                            //Change region
+                            locationManager.region = MKCoordinateRegion(
+                                center: CLLocationCoordinate2D(latitude: item.latitude, longitude: item.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
                         }, label: {
                             Text(item.label)
                         })
